eed1c803f2cba860d2a59aaf7261c7cd38e61113
fuadyassin <fuad.yassin@ec.gc.ca>
2024-10-09 14:47:51 -0600

    Update setup.py
    Changed the name of the package to match the root name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='meshpyProcessing',
+    name='MESH-Scripts-PyLib',
     version='0.1',
     packages=find_packages(),
     install_requires=[
