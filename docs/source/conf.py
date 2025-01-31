1e1cab3a8f235e08c8daea76d10cdfc0f870258c
fuadyassin <fuad.yassin@ec.gc.ca>
2025-01-30 18:36:18 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 # -- Project information -----------------------------------------------------
 
 project = 'MESH-Scripts-PyLib'
-copyright = f'{datetime.datetime.now().year}, Uchechukwu Udenze'
+copyright = f'{datetime.datetime.now().year}, Fuad Yassin'
 author = 'Fuad Yassin'
 release = "1"
 
