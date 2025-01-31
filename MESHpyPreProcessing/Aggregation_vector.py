df8973ab3cf923148ec5eb21938166f6732b0c5a
fuadyassin <fuad.yassin@ec.gc.ca>
2025-01-31 15:14:42 -0600

    Update Aggregation_vector.py

--- MESHpyPreProcessing/Aggregation_vector.py
+++ MESHpyPreProcessing/Aggregation_vector.py
@@ -21,29 +21,29 @@ river lengths, and weighted slopes for simplified river networks.
 Example usage:
 --------------
 
-from MESHpyPreProcessing.Aggregation_vector import merit_basin_aggregation
-import geopandas as gpd
-import os
+>>> from MESHpyPreProcessing.Aggregation_vector import merit_basin_aggregation
+>>> import geopandas as gpd
+>>> import os
 
-# Define paths and parameters
-input_basin_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/SrsAboine-geofabric/sras_subbasins_MAF_noAgg.shp"
-input_river_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/SrsAboine-geofabric/sras_rivers_MAF_noAgg.shp"
-min_subarea = 50
-min_slope = 0.0000001
-min_length = 1.0
-output_basin_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/sras_subbasins_MAF_Agg.shp"
-output_river_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/sras_rivers_MAF_Agg.shp"
+>>> # Define paths and parameters
+>>> input_basin_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/SrsAboine-geofabric/sras_subbasins_MAF_noAgg.shp"
+>>> input_river_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/SrsAboine-geofabric/sras_rivers_MAF_noAgg.shp"
+>>> min_subarea = 50
+>>> min_slope = 0.0000001
+>>> min_length = 1.0
+>>> output_basin_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/sras_subbasins_MAF_Agg.shp"
+>>> output_river_path = "/home/fuaday/github-repos/Souris_Assiniboine_MAF/1-geofabric/sras_rivers_MAF_Agg.shp"
 
-# Load input data
-input_basin = gpd.read_file(input_basin_path)
-input_river = gpd.read_file(input_river_path)
+>>> # Load input data
+>>> input_basin = gpd.read_file(input_basin_path)
+>>> input_river = gpd.read_file(input_river_path)
 
-# Perform aggregation
-agg_basin, agg_river = merit_basin_aggregation(input_basin, input_river, min_subarea, min_slope, min_length)
+>>> # Perform aggregation
+>>> agg_basin, agg_river = merit_basin_aggregation(input_basin, input_river, min_subarea, min_slope, min_length)
 
-# Save aggregated data
-agg_basin.to_file(output_basin_path)
-agg_river.to_file(output_river_path)
+>>> # Save aggregated data
+>>> agg_basin.to_file(output_basin_path)
+>>> agg_river.to_file(output_river_path)
 """
 import pandas as pd
 import geopandas as gpd
