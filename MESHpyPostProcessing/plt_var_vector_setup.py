c674a8cde0cdd8ddb36bcbd72ba7b8668bcff148
fuadyassin <fuad.yassin@ec.gc.ca>
2025-01-31 19:45:01 -0600

    Update plt_var_vector_setup.py

--- MESHpyPostProcessing/plt_var_vector_setup.py
+++ MESHpyPostProcessing/plt_var_vector_setup.py
@@ -60,6 +60,7 @@ Parameters
 - soldim : str, optional (default='nsol')
     NetCDF dimension representing different soil layers.
 """
+
 import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
