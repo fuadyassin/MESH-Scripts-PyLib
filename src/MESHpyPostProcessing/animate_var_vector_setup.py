f06a49ca88f5b7dd34cff876b3994125f753a2a1
fuadyassin <fuad.yassin@ec.gc.ca>
2025-06-11 17:30:06 -0600

    Update animate_var_vector_setup.py
    Removed first time step skip

--- src/MESHpyPostProcessing/animate_var_vector_setup.py
+++ src/MESHpyPostProcessing/animate_var_vector_setup.py
@@ -144,8 +144,8 @@ def animate_mesh_outputs_to_gif(
         time_units = time_units.replace('years', 'days')  # fix units if needed
         calendar = getattr(time_var, 'calendar', 'standard')
         dates = nc.num2date(times, units=time_units, calendar=calendar)
-        starting_date = dates[1]
-        date_range_length = len(dates) - 1
+        starting_date = dates[0]
+        date_range_length = len(dates)
 
     # Determine global min/max for each variable
     global_min_max = {}
@@ -159,7 +159,7 @@ def animate_mesh_outputs_to_gif(
         def animate_frame(date_index):
             ax.clear()
             with nc.Dataset(os.path.join(netcdf_dir, filenames[i])) as ds:
-                values = ds.variables[varnames[i]][date_index + 1, :]
+                values = ds.variables[varnames[i]][date_index, :]
 
             merge_df = shp.copy()
             df['value'] = values
